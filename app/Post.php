c3ef52c6cebd911c68ce72844111b9ea47bdbb53
Amira <miramiromiral@gmail.com>
2018-10-02 20:01:01 +0200

    database seeds

--- app/Post.php
+++ app/Post.php
@@ -6,15 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $fillable = [
-        'title',
-        'description',
-        'user_id',
-        'category_id'
-    ];
-
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
+    //
 }
